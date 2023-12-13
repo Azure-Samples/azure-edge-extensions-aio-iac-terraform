4be2992ce28fd37f8958b105e73669ebc97ae1ab
Allen Greaves <allengreaves@microsoft.com>
2023-12-13 12:16:06 -0800

    removing customlocation extension install

--- deploy/1-infra/register-providers.tf
+++ deploy/1-infra/register-providers.tf
@@ -18,7 +18,6 @@ resource "terraform_data" "register_providers" {
   count = var.should_register_azure_providers ? 1 : 0
   provisioner "local-exec" {
     command = <<-EOT
-      az extension add --upgrade --name customlocation -y
       %{for pr in local.providers_to_register~}
       echo "Registering ${pr}..."
       az provider register -n "${pr}" --wait
